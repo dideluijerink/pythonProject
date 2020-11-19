3d1536930bd364e41e2b521d354cd1f6c4754a5a
Your Name <you@example.com>
2020-11-19 18:27:50 +0100

    An example of debugging

---
+++ week_2/exercises_week_2.py
@@ -0,0 +1,14 @@
+message = type("All work but no play makes jack dull")
+print(message)
+
+print(6 * (1 - 2))
+
+bruce = 6
+print(bruce + 4)
+
+principal_amount = 1000
+interest_rate = 0.01
+nmbr_years = 5
+payout_frequency = 1
+print(final_amount = principal_amount (1 + (interest_rate / payout_frequency))**(payout_frequency * nmbr_years))
+
---
+++ week_2/variables.py
@@ -0,0 +1,13 @@
+this_is_a_variable = 15
+print(this_is_a_variable)
+print(15 + 6)
+
+result = this_is_a_variable + 6
+print(result)
+
+speed_in_kmh = 15
+
+print(type(speed_in_kmh))
+
+speed_in_kmh = int(input("What is the speed in KM/h?"))
+print(type(speed_in_kmh))
